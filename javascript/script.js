70f508d46f2479815fca652f8651d17f51118465
tightskinbe <wowmeupeight@gmail.com>
2019-11-28 15:44:34 -0800

    fixed some bad html and added some styleing slider size

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const emailForm = document.getElementById('form');
 
 
+    emailForm.addEventListener('submit', function (event) {
+        event.preventDefault()
+        const emailBtn = document.getElementById('email').value;
+        emailValue(emailBtn);
+    })
 
-
-
-
+    function emailValue(email) {
+        if (email === '') {
+            alert('Invalid Email');
+        } else {
+            alert('Thanks For Subscribing!');
+        }
+    }
 
 });    //end of doc ready
